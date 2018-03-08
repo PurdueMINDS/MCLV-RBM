c03be7a4abcf07b40eae99de11884c3430cf5104
Mayank Kakodkar <mkakodka@purdue.edu>
2018-03-08 00:52:20 -0500

    Live Run

--- sh/run.sh
+++ sh/run.sh
@@ -3,19 +3,19 @@ export bf='/scratch-data/mkakodka/mclv_aaai18/'
 export rb="$bf/py/main.py"
 
 nhup "python3 $rb --base-folder $bf \
---sqlite aaai_trial \
+--sqlite AAAI \
 --double \
 --log-tour \
 --gpu-limit 17 \
 --no-discrete \
 --phase DATA \
---runs 1 \
---total-epochs 2 \
---warmup-epochs 1 \
+--runs 10 \
+--total-epochs 100 \
+--warmup-epochs 15 \
 --num-hidden 25 32 \
 --method MCLV CD PCD \
 --k 1 3 10 \
 --batch-size 128 \
 --learning-rate 1.0 0.1 0.01 0.001 \
 --schedule RM10  \
---gpu-id 0 " "AAAI_TRIAL"
+--gpu-id 0 " "AAAI"
